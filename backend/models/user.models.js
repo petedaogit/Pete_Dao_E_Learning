ad9fda17131a30140ad7c8ef6164b2e7dfd959ce
petedaogit <namdao.0908@gmail.com>
2025-05-07 17:29:53 +0700

    update id in user

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    id: {
+    _id: {
       type: String,
       required: true,
     },
@@ -13,7 +13,6 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
     },
     imageUrl: {
       type: String,
